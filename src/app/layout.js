41491aa43cde2667a71e4b8da985a7f6f148e5c1
yash-sangani <sanganiyash007@gmail.com>
2024-05-03 14:10:10 +0530

    navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { cx } from "@/utils";
+import { cx } from "../utils";
 import "./globals.css";
 import { Inter, Manrope } from "next/font/google";
-import Header from "@/components/Header";
+import Header from "../components/Header";
 
 const inter = Inter({
   subsets: ["latin"],
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,22 @@
 import Link from "next/link"
 import Logo from "./Logo"
+import { DribbbleIcon, GithubIcon, LinkedinIcon, SunIcon, TwitterIcon } from "../icons"
 
 const Header = () => {
   return (
     <header className="w-full p-4 px-10 flex items-center justify-between ">
         <Logo />
-        <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center">
+        <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm">
             <Link href='/' className="mr-2">Home</Link>
             <Link href='/about'  className="mx-2">About</Link>
             <Link href='/contact'  className="mr-2">Contact</Link>
-            <button></button>
+            <button><SunIcon /></button>
         </nav>
         <div>
-            Social Links
+           <a href="#" className="inline-block w-6 h-6 mr-4 hover:scale-125 transition-all ease duration-200"><LinkedinIcon className=""/></a>
+           <a href="#" className="inline-block w-6 h-6 mr-4 hover:scale-125 transition-all ease duration-200"><TwitterIcon className=""/></a>
+           <a href="#" className="inline-block w-6 h-6 mr-4 hover:scale-125 transition-all ease duration-200"><GithubIcon className=""/></a>
+           <a href="#" className="inline-block w-6 h-6 mr-4 hover:scale-125 transition-all ease duration-200"><DribbbleIcon className=""/></a>
         </div>
     </header>
   )
