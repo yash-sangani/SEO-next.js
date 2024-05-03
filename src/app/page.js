e59d70f38a465f243564a1857dd782b4b62f5f44
yash-sangani <sanganiyash007@gmail.com>
2024-05-03 15:38:42 +0530

    render the blogs

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
-
+import {allBlogs} from "contentlayer/generated"
+ 
 export default function Home() {
+  console.log(allBlogs);
   return (
     <main className="flex flex-col items-center justify-center">
       Hello World
