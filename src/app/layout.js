652eab1ff3d90712b428ca170e8cac26e0793c2c
yash-sangani <sanganiyash007@gmail.com>
2024-05-03 15:05:51 +0530

    setup content layer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { cx } from "../utils";
+import { cx } from "@/src/utils";
 import "./globals.css";
 import { Inter, Manrope } from "next/font/google";
-import Header from "../components/Header";
+import Header from "@/src/components/Header";
 
 const inter = Inter({
   subsets: ["latin"],
