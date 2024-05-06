5ca1dfa8a777382e255b0fd9c6fd47913cfdddef
yash-sangani <sanganiyash007@gmail.com>
2024-05-06 13:07:40 +0530

    Contact page

---
+++ src/components/Contact/ContactForm.js
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="mt-12 text-xl font-medium leading-relaxed font-in"
+    >
+      Hello! My name is{" "}
+      <input
+        type="text"
+        placeholder="your name"
+        {...register("name", { required: true, min: 1, maxLength: 80 })}
+        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray focus:border-gray bg-transparent"
+      />
+      and I want to discuss a potential. You can email me at{" "}
+      <input type="email" placeholder="your@email" {...register}
+       className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray focus:border-gray bg-transparent"
+      />
+      or reach out to me on{" "}
+      <input
+        type="tel"
+        placeholder="your phone"
+        {...register("phone number", {})}
+        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray focus:border-gray bg-transparent"
+      />
+      Here are some details about my project:<br />
+      <textarea {...register("project detaills", {})} 
+      placeholder="My project is about..."
+      rows={3}
+       className="w-full outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-lg border-b border-gray focus:border-gray bg-transparent"/>
+      <input type="submit" value="send request" className="mt-8 font-medium inline-block capitalize text-xl py-3 px-8 border-2 border-solid border-dark rounded cursor-pointer"/>
+    </form>
+  );
+}
